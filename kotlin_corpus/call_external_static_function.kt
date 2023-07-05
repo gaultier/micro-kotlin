e16c2ff61a9dae71e80eac5470035422ad1194f6
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-07-05 17:33:11 +0200

    kt

---
+++ kotlin_corpus/call_external_static_function.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+  if (System.nanoTime() > 0){
+    var foo : Int = 10
+  }
+}
---
+++ kotlin_corpus/var_declared_within_if.kt
@@ -0,0 +1,6 @@
+fun main() {
+  if (true){
+    var foo : Int = 10
+  }
+  println(false)
+}
