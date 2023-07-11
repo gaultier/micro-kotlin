9095ca8dda906e31c5f9f59526aeb954e847bd17
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-07-11 17:26:47 +0200

    add kotlin files

---
+++ kotlin_corpus/equal_equal.kt
@@ -0,0 +1,14 @@
+fun main() {
+  var a: Int = 3
+  var b: Int = 3
+  var c: Int = 4
+  if (a==b) {
+    println(10)
+  } else if (a==c) {
+    println(11)
+  } else if(b==c) {
+    println(12)
+  } else {
+    println(13)
+  }
+}
