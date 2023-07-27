9767be284fe46b98a7ba16d9b36425697ed99242
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-07-27 23:18:41 +0200

    Start building method type

--- class_file.h
+++ class_file.h
@@ -6046,7 +6046,7 @@ static u32 ty_resolve_node(resolver_t *resolver, u32 node_i, arena_t *arena) {
 
     return node->type_i = pg_array_last_index(resolver->parser->types);
   }
-  case AST_KIND_FUNCTION_DEFINITION:
+  case AST_KIND_FUNCTION_DEFINITION: {
     ty_begin_scope(resolver);
     // Arguments (lhs).
     ty_resolve_node(resolver, node->lhs, arena);
@@ -6054,13 +6054,16 @@ static u32 ty_resolve_node(resolver_t *resolver, u32 node_i, arena_t *arena) {
     ty_resolve_node(resolver, node->rhs, arena);
     ty_end_scope(resolver);
 
+    const par_ast_node_t *const lhs = &resolver->parser->nodes[node->lhs];
+    pg_assert(lhs->kind == AST_KIND_LIST);
+
     // FIXME
     const ty_type_t void_type = {.kind = TYPE_KOTLIN_UNIT};
     const ty_type_t type = {
         .kind = TYPE_KOTLIN_METHOD,
         .v = {.method =
                   {
-                      .argument_count = 0,
+                      .argument_count = pg_array_len(lhs->nodes),
                       .argument_types_i = 0,
                       .return_type_i = ty_add_type(&resolver->parser->types,
                                                    &void_type, arena),
@@ -6068,6 +6071,7 @@ static u32 ty_resolve_node(resolver_t *resolver, u32 node_i, arena_t *arena) {
     };
     pg_array_append(resolver->parser->types, type, arena);
     return node->type_i = pg_array_last_index(resolver->parser->types);
+  }
 
   case AST_KIND_VAR_DEFINITION: {
     pg_assert(node->lhs > 0); // TODO: The type is actually optional.
