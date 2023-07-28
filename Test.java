f30d209fc0bf98e194c47bfda32aa6d1e120818e
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-07-28 09:22:01 +0200

    test kotlin method in java

--- Test.java
+++ Test.java
@@ -1,12 +1,5 @@
 class Test {
-  public static int x = 99;
-  public static final Foo foo = new Foo(3);
-
   public static void main(String[] args) {
-    System.out.println(Test.foo);
-  }
-
-  int plus(int a, int b) {
-    return a + b;
+    System.out.println(DemoKt.plus(3L, 255L));
   }
 }
