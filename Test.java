d2d9e666b5aa701f23d707e8db9c991ee7ee7af2
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-08-02 17:34:32 +0200

    clean up test.java

--- Test.java
+++ Test.java
@@ -2,16 +2,9 @@ import java.net.http.HttpClient;
 
 class Test {
   public static void main(String[] args) {
-
-   HttpClient client = HttpClient.newBuilder()
-        .build();
-    // System.out.println(foo.bar.Baz.math(11, 10));
-
-    // DemoKt.fibonacci(10);
-    // DemoKt.fibonacci_10();
-    // DemoKt.fibonacci_10_long();
-    // int a = TestKt.square(5);
-    // Integer b = TestKt.square(6);
+    DemoKt.fibonacci(10);
+    DemoKt.fibonacci_10();
+    DemoKt.fibonacci_10_long();
   }
 
   public static int square_primitive(int x) {
