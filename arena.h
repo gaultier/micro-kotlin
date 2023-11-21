6084147c32d26bc99fb739af8653e6319acf9926
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-11-21 11:37:19 +0100

    typedef instead of macros

--- arena.h
+++ arena.h
@@ -10,14 +10,14 @@
 #include <string.h>
 #include <sys/mman.h>
 
-#define u64 uint64_t
-#define i64 int64_t
-#define u32 uint32_t
-#define i32 int32_t
-#define u16 uint16_t
-#define i16 int16_t
-#define u8 uint8_t
-#define i8 int8_t
+typedef uint64_t u64;
+typedef int64_t i64;
+typedef uint32_t u32;
+typedef int32_t i32;
+typedef uint16_t u16;
+typedef int16_t i16;
+typedef uint8_t u8;
+typedef int8_t i8;
 typedef size_t usize;
 typedef ssize_t isize;
 
